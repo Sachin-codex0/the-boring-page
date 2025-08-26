96cb71bad19088996cbbcf0a005bf6a649ed96a3
Sachin Jha <sachinjhagc@gmail.com>
2025-08-26 23:27:26 +0530

    uploaded from github projects folder
    the webpage contains random facts, jokes & quotes built using some APIs

---
+++ app.js
@@ -0,0 +1,179 @@
+let catFactBtn = document.querySelector(".fact-btn");
+let sideBar = document.querySelector(".side-bar");
+let earthBtn = document.querySelector(".earth-facts");
+let uselessFactBtn = document.querySelector(".usels-facts");
+let jokeBtn = document.querySelector(".joke-btn");
+let quoteBtn = document.querySelector(".quote-btn");
+let heading = document.querySelector(".heading");
+let firstLine = document.querySelector(".first-line");
+let secLine = document.querySelector(".second-line");
+let getMore = document.querySelector(".more");
+
+let cat_fact = false; let earth_fact = false; let useless_fact = false; let coding_jokes = false; let GOT_quotes = false;
+
+getMore.addEventListener("click", function () {
+    if (cat_fact) {
+        randomCatFact();
+        secLine.innerText = "";
+    } else if (earth_fact) {
+        earthFacts();
+        secLine.innerText = "";
+    } else if (useless_fact) {
+        uselessFact();
+        secLine.innerText = "";
+    } else if (coding_jokes) {
+        programmingJokes();
+    } else if (GOT_quotes) {
+        GOTquotes();
+    }
+})
+
+catFactBtn.addEventListener("click", function () {
+    sideBar.style.display = "block";
+    heading.innerText = "Random Cat Facts";
+    randomCatFact();
+    secLine.innerText = "";
+    cat_fact = true;
+    earth_fact = false;
+    useless_fact = false;
+    GOT_quotes = false;
+});
+
+earthBtn.addEventListener("click", function () {
+    sideBar.style.display = "block";
+    heading.innerText = "Random Earth Facts";
+    earthFacts();
+    secLine.innerText = "";
+    earth_fact = true;
+    cat_fact = false;
+    useless_fact = false;
+    GOT_quotes = false;
+});
+
+uselessFactBtn.addEventListener("click", function () {
+    sideBar.style.display = "block";
+    heading.innerText = "Random Facts";
+    uselessFact();
+    secLine.innerText = "";
+    useless_fact = true;
+    cat_fact = false;
+    earth_fact = false;
+    GOT_quotes = false;
+});
+
+jokeBtn.addEventListener("click", function () {
+    sideBar.style.display = "block";
+    heading.innerText = "Programming Jokes";
+    programmingJokes();
+    getMore.innerText = "more jokes";
+    coding_jokes = true;
+    useless_fact = false;
+    cat_fact = false;
+    earth_fact = false;
+    GOT_quotes = false;
+});
+
+quoteBtn.addEventListener("click", function () {
+    sideBar.style.display = "block";
+    heading.innerText = "Game of Thrones Quotes";
+    GOTquotes();
+    getMore.innerText = "more quotes";
+    useless_fact = false;
+    cat_fact = false;
+    earth_fact = false;
+    coding_jokes = false;
+    GOT_quotes = true;
+});
+
+let backBtn = document.querySelector(".backBtn");
+backBtn.addEventListener("click", function () {
+    sideBar.style.display = "none";
+});
+
+// Random Cat Facts
+let urlForCat = "https://catfact.ninja/fact";
+function randomCatFact() {
+    fetch(urlForCat)
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            firstLine.innerText = `"${data.fact}"`;
+        })
+        .catch((e) => {
+            firstLine.innerText = "You've reached max limit";
+            secLine.innerText = "";
+        })
+}
+
+// Random Earth Facts
+let earthUrl = "https://api.bootprint.space/all/earth";
+function earthFacts() {
+    fetch(earthUrl)
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            firstLine.innerText = `"${data.fact}"`;
+        })
+        .catch((e) => {
+            firstLine.innerText = "You've reached max limit";
+            secLine.innerText = "";
+        })
+}
+
+// useless facts 
+let uselessFactUrl = "https://uselessfacts.jsph.pl/api/v2/facts/random";
+function uselessFact() {
+    fetch(uselessFactUrl)
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            firstLine.innerText = `"${data.text}"`;
+        })
+        .catch((e) => {
+            firstLine.innerText = "You've reached max limit";
+            secLine.innerText = "";
+        })
+}
+
+// Random Programming Jokes
+let codingJokesUrl = "https://v2.jokeapi.dev/joke/Programming";
+function programmingJokes() {
+    fetch(codingJokesUrl)
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            console.log(data);
+            if (data.type == "twopart") {
+                firstLine.innerText = data.setup;
+                secLine.innerText = `:- ${data.delivery}`;
+            } else if (data.type == "single") {
+                firstLine.innerText = data.joke;
+                secLine.innerText = "";
+            }
+        })
+        .catch((e) => {
+            firstLine.innerText = "You've reached max limit";
+            secLine.innerText = "";
+        })
+}
+
+// Game of Thrones quotes
+let GOTUrl = "https://api.gameofthronesquotes.xyz/v1/random";
+function GOTquotes() {
+    fetch(GOTUrl)
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            firstLine.innerText = `"${data.sentence}"`;
+            secLine.innerText = `:- ${data.character.name}`;
+        })
+        .catch((e) => {
+            firstLine.innerText = "You've reached max limit";
+            secLine.innerText = "";
+        })
+}
